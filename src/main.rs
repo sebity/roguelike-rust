e1c11f2674e473421d3bfdec175c8c1871c17cbc
Jan Tatham <jan@sebity.com>
2016-07-23 22:32:24 +0100

    Updated: font path

--- src/main.rs
+++ src/main.rs
@@ -73,7 +73,7 @@ fn handle_keys(root: &mut Root, player: &mut Object) -> bool {
 
 fn main() {
     let mut root = Root::initializer()
-        .font("./arial10x10.png", FontLayout::Tcod)
+        .font("data/fonts/arial10x10.png", FontLayout::Tcod)
         .font_type(FontType::Greyscale)
         .size(SCREEN_WIDTH, SCREEN_HEIGHT)
         .title("Rust/libtcod tutorial")
